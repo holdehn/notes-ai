1b347eb7984357b161aeb13688746969ba6a55bf
musiklinkco <holdenmeyer@gmail.com>
2023-04-30 14:06:48 -0700

    secure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,16 +16,28 @@ const montserrat = Montserrat({
 function MyApp({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
 
+  const isSecure = process.env.NODE_ENV === 'production';
+
   supabase.auth.onAuthStateChange((event, session) => {
     if (event === 'SIGNED_OUT' || event === 'USER_DELETED') {
       // delete cookies on sign out
       const expires = new Date(0).toUTCString();
-      document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
+      document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=${
+        isSecure ? 'Secure' : 'Lax'
+      }; secure`;
       document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
     } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
       const maxAge = 100 * 365 * 24 * 60 * 60; // 100 years, never expires
-      document.cookie = `my-access-token=${session?.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
-      document.cookie = `my-refresh-token=${session?.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
+      document.cookie = `my-access-token=${
+        session?.access_token
+      }; path=/; max-age=${maxAge}; SameSite=${
+        isSecure ? 'Secure' : 'Lax'
+      }; secure`;
+      document.cookie = `my-refresh-token=${
+        session?.refresh_token
+      }; path=/; max-age=${maxAge}; SameSite=${
+        isSecure ? 'Secure' : 'Lax'
+      }; secure`;
     }
   });
   return (
