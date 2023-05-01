4c89b38c03b8f3cf199e95fef1949e973d6052cb
musiklinkco <holdenmeyer@gmail.com>
2023-04-30 18:26:21 -0700

    fml

--- pages/my-notes/index.tsx
+++ pages/my-notes/index.tsx
@@ -1,36 +1,50 @@
-import {
-  GetServerSidePropsContext,
-  NextApiRequest,
-  NextApiResponse,
-} from 'next';
+import { GetServerSidePropsContext } from 'next';
 import { parseCookies } from 'nookies';
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import Head from 'next/head';
 import { SWRConfig } from 'swr';
 import NotesComponent from '@/components/NotesComponent/NotesComponent';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-export const getServerSideProps = async (
-  ctx:
-    | GetServerSidePropsContext
-    | { req: NextApiRequest; res: NextApiResponse },
-) => {
-  // Create authenticated Supabase Client
-  const supabase = createServerSupabaseClient(ctx);
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const cookies = parseCookies(ctx);
+  const accessToken = cookies['my-access-token'];
+  const refreshToken = cookies['my-refresh-token'];
 
-  if (!session)
+  if (!accessToken || !refreshToken) {
     return {
       redirect: {
         destination: '/',
         permanent: false,
       },
     };
+  }
+
+  if (accessToken && refreshToken) {
+    // Set the cookies
+    const maxAge = 100 * 365 * 24 * 60 * 60; // 100 years, never expires
+    ctx.res.setHeader('Set-Cookie', [
+      `my-access-token=${accessToken}; Path=/; Max-Age=${maxAge}; SameSite=Lax; Secure; HttpOnly`,
+      `my-refresh-token=${refreshToken}; Path=/; Max-Age=${maxAge}; SameSite=Lax; Secure; HttpOnly`,
+    ]);
+  }
+  const supabase = createServerSupabaseClient(ctx);
+  await supabase.auth.setSession({
+    access_token: accessToken,
+    refresh_token: refreshToken,
+  });
 
   const user = await supabase.auth.getUser();
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   // Fetch notes-page-data using the supabaseClient
   const { data: userData, error: userError } = await supabase
@@ -64,11 +78,15 @@ export const getServerSideProps = async (
           notes: notesData,
           sessions: sessionData,
         },
+        accessToken,
+        refreshToken,
       },
     },
   };
 };
 export default function ({ fallback }: { fallback: any }) {
+  const router = useRouter();
+
   return (
     <>
       <Head>
