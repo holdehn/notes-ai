eebc899491a9a0278c0371d73735edab3b71c30f
musiklinkco <holdenmeyer@gmail.com>
2023-05-03 14:39:36 -0700

    now it work?

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin',
           },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
         ],
       },
     ];
