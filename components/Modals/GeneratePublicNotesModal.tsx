ad6e815597a202800ee55993487a64d4831b3ca7
musiklinkco <holdenmeyer@gmail.com>
2023-05-21 18:17:11 -0700

    new homepage

--- components/Modals/GeneratePublicNotesModal.tsx
+++ components/Modals/GeneratePublicNotesModal.tsx
@@ -49,8 +49,6 @@ export default function GeneratePublicNotesModal(props: Props) {
 
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
-
-      // Check if the uploaded file is an audio, video, or PDF file
       const fileType = file.type.split('/')[0];
       const fileSubType = file.type.split('/')[1];
 
@@ -58,9 +56,12 @@ export default function GeneratePublicNotesModal(props: Props) {
         (fileType !== 'audio' &&
           fileType !== 'video' &&
           fileType !== 'application') ||
-        (fileType === 'application' && fileSubType !== 'pdf')
+        (fileType === 'application' &&
+          fileSubType !== 'pdf' &&
+          fileSubType !==
+            'vnd.openxmlformats-officedocument.wordprocessingml.document') // Add check for docx file
       ) {
-        alert('Please upload an audio, video, or PDF file');
+        alert('Please upload an audio, video, PDF, or docx file');
         return;
       }
 
@@ -70,10 +71,10 @@ export default function GeneratePublicNotesModal(props: Props) {
       };
       setFiles(fileDisplay);
       setFileObject(file);
-
       setNextId(nextId + 1);
     }
   };
+
   const removeFile = () => {
     setFiles(null);
     setFileObject(null);
@@ -184,6 +185,35 @@ export default function GeneratePublicNotesModal(props: Props) {
     }
   };
 
+  const loadDocx = async (file: File) => {
+    try {
+      if (!file) {
+        alert('Please upload a docx file');
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch('/api/load-docx', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      const extractedText = data.text;
+      setConvertedText(extractedText);
+      return extractedText;
+    } catch (error: any) {
+      console.log(JSON.stringify(error));
+      alert(`Error: ${error.message}`);
+    }
+  };
+
   //formik validation
   const validationSchema = Yup.object({
     title: Yup.string().required('Required'),
@@ -205,10 +235,18 @@ export default function GeneratePublicNotesModal(props: Props) {
     const noteID = uuidv4();
     const file = fileObject;
     const fileType = file.type.split('/')[0];
+    const fileSubType = file.type.split('/')[1]; // define fileSubType here
     let transcription;
 
     if (fileType === 'application') {
-      transcription = await loadPDF(file);
+      if (
+        fileSubType ===
+        'vnd.openxmlformats-officedocument.wordprocessingml.document'
+      ) {
+        transcription = await loadDocx(file);
+      } else if (fileSubType === 'pdf') {
+        transcription = await loadPDF(file);
+      }
     } else if (fileType === 'video') {
       // If the file is a video, convert it to audio and then send it to sendAudio
       const audioFile = await convertVideoToMp3(file);
